b40aa344836250cac58b3bcac766cada17d2dc24
HuynhQui01 <huynhqui309@gmail.com>
2023-02-22 09:38:14 +0700

    Create MainController and make homepage

---
+++ src/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/homepage", name="homepageForm")
+     */
+    public function Homepage(): Response
+    {
+        // $products = $pro->findAllProduct_8item();
+        // $brand = $bra->findAll();
+        return $this->render('main/index.html.twig', [
+            // 'products' => $products,
+            // 'brand' => $brand
+        ]);
+    }
+}
