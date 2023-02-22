5f2c6a0361c420b0d8b87d14d914978dce396e77
HuynhQui01 <huynhqui309@gmail.com>
2023-02-22 10:38:59 +0700

    commit 4: add function in Main

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\BrandsRepository;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,24 +13,37 @@ class MainController extends AbstractController
     /**
      * @Route("/homepage", name="homepageForm")
      */
-    public function Homepage(): Response
+    public function Homepage(ProductsRepository $pro, BrandsRepository $bra): Response
     {
-        // $products = $pro->findAllProduct_8item();
-        // $brand = $bra->findAll();
+        $products = $pro->findAllProduct_8item();
+        $brand = $bra->findAll();
         return $this->render('main/index.html.twig', [
-            // 'products' => $products,
-            // 'brand' => $brand
+            'products' => $products,
+            'brand' => $brand
         ]);
     }
 
-    /**
+        /**
      * @Route("/aboutus", name="aboutusForm")
      */
-    public function Aboutus(): Response
+    public function Aboutus(BrandsRepository $bra): Response
     {
-        // $brand = $bra->findAll();
+        $brand = $bra->findAll();
         return $this->render('main/aboutus.html.twig', [
-            // 'brand' => $brand
+            'brand' => $brand
+        ]);
+    }
+
+
+
+        /**
+     * @Route("/contact", name="contactForm")
+     */
+    public function Contact(BrandsRepository $bra): Response
+    {
+        $brand = $bra->findAll();
+        return $this->render('main/contact.html.twig', [
+            'brand' => $brand
         ]);
     }
 }
