42503e82544182fd62bac3b72280cf3fc4b88d1b
HuynhQui01 <huynhqui309@gmail.com>
2023-02-23 08:57:00 +0700

    commit 5: add showProductBrand function

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\BrandsRepository;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +17,21 @@ class ProductsController extends AbstractController
     {
         return $this->render('products/index.html.twig', [
             'controller_name' => 'ProductsController',
+        ]);
+    }
+
+        /**
+     * @Route("/product/brand/{id}", name="product_brand")
+     */
+    public function showProductBrand(ProductsRepository $rePro, BrandsRepository $reBra, int $id): Response
+    {
+        $brand = $reBra->findOneBrand($id);
+        $br = $reBra->findAll();
+        $pro = $rePro->findProductBrand($id);
+        return $this->render('product/findBrand.html.twig', [
+            'brand' => $br,
+            'bra' => $brand,
+            'product' => $pro
         ]);
     }
 }
