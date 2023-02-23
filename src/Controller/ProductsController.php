69cdc9822bd8bfb9f00d4fa56d37f62a1174cec9
thanhhao1903 <HaoLTGCC210232@fpt.edu.vn>
2023-02-23 10:11:20 +0700

    Hao commit 1: make controller Search products and templates

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,19 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
+use App\Repository\BrandsRepository;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductsController extends AbstractController
 {
     /**
-     * @Route("/products", name="app_products")
+     * @Route("/product/detail/{id}", name="detail_product")
      */
-    public function index(): Response
+    public function showDetailProduct(Products $pro, ProductsRepository $repo, BrandsRepository $bra): Response
     {
-        return $this->render('products/index.html.twig', [
-            'controller_name' => 'ProductsController',
+        $brid = $pro->getBrand();
+        $brand = $repo->findRelativeProduct($brid);
+        $br = $bra->findAll();
+        return $this->render('product/detail.html.twig', [
+            'product'=>$pro,
+            'brid'=>$brand,
+            'brand'=>$br
+        ]);
+    }
+
+        /**
+     * @Route("/product/brand/{id}", name="product_brand")
+     */
+    public function showProductBrand(ProductsRepository $rePro, BrandsRepository $reBra, int $id): Response
+    {
+        $brand = $reBra->findOneBrand($id);
+        $br = $reBra->findAll();
+        $pro = $rePro->findProductBrand($id);
+        return $this->render('product/findBrand.html.twig', [
+            'brand' => $br,
+            'bra' => $brand,
+            'product' => $pro
+        ]);
+    }
+
+        /**
+     * @Route("/product/find", name="product_find")
+     */
+    public function searchProduct(ProductsRepository $rePro, BrandsRepository $reBra, Request $req): Response
+    {
+        $kw = $req->query->get("keyword");
+        $br = $reBra->findAll();
+
+        $product = $rePro->findAll();
+
+        $product = $rePro->findProduct($kw);
+        return $this->render('product/findProduct.html.twig', [
+            'brand' => $br,
+            'product' => $product,
+            'kw' => $kw
         ]);
     }
 }
