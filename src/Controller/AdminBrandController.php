2528b104e29669db56d18b02685edf489ac44ce7
khoilvgcc210040 <khoilvgcc210040@fpt.edu.vn>
2023-02-23 08:48:00 +0700

    Khoi commit 2: add new brand in controller and template

--- src/Controller/AdminBrandController.php
+++ src/Controller/AdminBrandController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Brands;
 use App\Repository\BrandsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,6 +19,23 @@ class AdminBrandController extends AbstractController
         $brand = $repo->findAll();
         return $this->render('admin_brand/list.html.twig', [
             'brand' => $brand
+        ]);
+    }
+
+    /**
+     * @Route("admin/brand/add", name="add_brand")
+     */
+    public function addBrand(BrandsRepository $repo, Request $req): Response
+    {
+        $a = new Brands();
+        $form = $this->createForm(BrandType::class,$a);
+        $form->handleRequest($req);
+        if ($form->isSubmitted()&&$form->isValid()) {
+            $repo->add($a,true);
+            return $this->redirectToRoute('show_brand', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->render('admin_brand/add.html.twig', [
+            'form'=>$form->createView()
         ]);
     }
 }
