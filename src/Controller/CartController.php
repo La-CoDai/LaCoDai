bb6f8272723ec77361127d953ffdec48ed4689d2
khoilvgcc210040 <khoilvgcc210040@fpt.edu.vn>
2023-02-25 10:44:16 +0700

    Khoi commit 9: add product to cart function

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Cart;
+use App\Entity\Products;
 use App\Repository\BrandsRepository;
 use App\Repository\CartRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,5 +36,44 @@ class CartController extends AbstractController
             'product' => $product,
             'brand' => $brand
         ]);
+    }
+
+    /**
+     * @Route("/addcart/{id}", name="add_cart")
+     */
+    public function addcart(Products $pro, CartRepository $reCart, Request $req): Response
+    {
+        $quantity_temp = $req->query->get('quantity');
+        if ($quantity_temp > 1) {
+            $quantityNew = $quantity_temp;
+        }else {
+            $quantityNew = 1;
+        }
+        
+        $user = $this->getUser();
+        $data[]=[
+            'id'=>$user->getId()
+        ];
+        $uid = $data[0]['id'];
+
+        $cart = $reCart->findBy([
+            'procart'=>$pro->getId(),
+            'usercart'=>$uid
+        ]);
+
+        if (count($cart) == 0) {
+            $c = new Cart();
+            $c->setProcart($pro);
+            $c->setQuantity($quantityNew);
+            $c->setUsercart($user);
+        }
+        else {   
+           $c = $reCart->find($cart[0]->getId()); // a Cart 
+           $quantityOld = $c->getQuantity();
+           $quantityNew = $quantityOld + $quantityNew;
+           $c->setquantity($quantityNew);
+       }
+        $reCart->add($c,true);
+        return $this->redirectToRoute('show_cart', [], Response::HTTP_SEE_OTHER);
     }
 }
